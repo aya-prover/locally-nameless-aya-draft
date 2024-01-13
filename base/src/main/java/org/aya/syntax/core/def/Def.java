4622634a77966766a3405982b2717f321b0de932
HoshinoTented <hoshinotented@qq.com>
2024-01-13 18:34:00 +0800

    term: def (unable to pass the compilation though)

--- base/src/main/java/org/aya/syntax/core/def/Def.java
+++ base/src/main/java/org/aya/syntax/core/def/Def.java
@@ -1,4 +1,25 @@
+// Copyright (c) 2020-2023 Tesla (Yinsen) Zhang.
+// Use of this source code is governed by the MIT license that can be found in the LICENSE.md file.
 package org.aya.syntax.core.def;
 
-public interface Def {
+import org.aya.generic.AyaDocile;
+import org.aya.pretty.doc.Doc;
+import org.aya.syntax.core.term.Term;
+import org.aya.syntax.ref.DefVar;
+import org.aya.util.prettier.PrettierOptions;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.function.Consumer;
+
+/**
+ * @author zaoqi
+ */
+public sealed interface Def extends AyaDocile permits ClassDef, TeleDef {
+  @NotNull DefVar<?, ?> ref();
+
+  void descentConsume(@NotNull Consumer<Term> f, @NotNull Consumer<Pat> g);
+
+  @Override default @NotNull Doc toDoc(@NotNull PrettierOptions options) {
+    throw new UnsupportedOperationException("TODO");
+  }
 }
