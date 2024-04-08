613a67aee8d515f8c3f175635edfacb6b9abffb1
ice1000 <ice1000kotlin@foxmail.com>
2024-04-08 19:55:40 -0400

    resolve: `BindBlock`

--- base/src/main/java/org/aya/resolve/ResolveInfo.java
+++ base/src/main/java/org/aya/resolve/ResolveInfo.java
@@ -7,6 +7,7 @@ import kala.collection.mutable.MutableMap;
 import org.aya.generic.TyckOrder;
 import org.aya.resolve.context.ModuleContext;
 import org.aya.resolve.salt.AyaBinOpSet;
+import org.aya.syntax.concrete.stmt.BindBlock;
 import org.aya.syntax.concrete.stmt.ModuleName;
 import org.aya.syntax.concrete.stmt.Stmt;
 import org.aya.syntax.concrete.stmt.UseHide;
@@ -23,25 +24,27 @@ public record ResolveInfo(
   // @NotNull PrimDef.Factory primFactory,
   // @NotNull AyaShape.Factory shapeFactory,
   @NotNull AyaBinOpSet opSet,
-  // @NotNull MutableMap<DefVar<?, ?>, Tuple3<RenamedOpDecl, BindBlock, Boolean>> opRename,
+  @NotNull MutableMap<DefVar<?, ?>, OpRenameInfo> opRename,
   @NotNull MutableMap<ModuleName.Qualified, ImportInfo> imports,
   @NotNull MutableMap<ModuleName.Qualified, UseHide> reExports,
   @NotNull MutableGraph<TyckOrder> depGraph
 ) {
-  public record ImportInfo(
-    @NotNull ResolveInfo resolveInfo,
-    boolean isHiding
-  ) {
+  public record ImportInfo(@NotNull ResolveInfo resolveInfo, boolean reExport) {
   }
 
-  /*
+  public record OpRenameInfo(@NotNull RenamedOpDecl renamed, @NotNull BindBlock bind, boolean reExport) {
+  }
+
+  /**
    * @param definedHere Is this operator renamed in this module, or publicly renamed by upstream?
    * @see #open(ResolveInfo, org.aya.util.error.SourcePos, Stmt.Accessibility)
    */
-  /*
-  public void renameOp(@NotNull DefVar<?, ?> defVar, @NotNull OpDecl renamed, @NotNull BindBlock bind, boolean definedHere) {
+  public void renameOp(@NotNull DefVar<?, ?> defVar, @NotNull RenamedOpDecl renamed, @NotNull BindBlock bind, boolean definedHere) {
     defVar.addOpDeclRename(thisModule.modulePath(), renamed);
-    opRename.put(defVar, Tuple.of(renamed, bind, definedHere));
+    opRename.put(defVar, new OpRenameInfo(renamed, bind, definedHere));
   }
-  */
+
+  @Debug.Renderer(text = "opInfo.name()")
+  public record RenamedOpDecl(@NotNull OpInfo opInfo) implements OpDecl {
+  }
 }
