0c0229ba4a6583985b267754697a950002c2503f
HoshinoTented <hoshinotented@qq.com>
2024-05-11 17:50:58 +0800

    conversion: LetTerm

--- base/src/main/java/org/aya/unify/TermComparator.java
+++ base/src/main/java/org/aya/unify/TermComparator.java
@@ -240,6 +240,9 @@ public abstract sealed class TermComparator extends AbstractTycker permits Unifi
       // it means their arguments don't match (even the ref don't match),
       // so we are unable to do more if we can't normalize them.
       case FnCall _ -> null;
+
+      // we don't need to compare LetTerm, since they always can be wh normalized.
+      case LetTerm _ -> throw noRules(lhs);
       default -> throw noRules(lhs);
     };
   }
