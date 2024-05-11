1189e064acd32b953091e00f6eedcc018915a28b
HoshinoTented <hoshinotented@qq.com>
2024-05-11 18:04:32 +0800

    misc: doc

--- base/src/main/java/org/aya/tyck/ctx/LocalSubstitution.java
+++ base/src/main/java/org/aya/tyck/ctx/LocalSubstitution.java
@@ -4,7 +4,6 @@ package org.aya.tyck.ctx;
 
 import kala.collection.mutable.MutableLinkedHashMap;
 import kala.control.Option;
-import org.aya.syntax.core.term.Term;
 import org.aya.syntax.ref.LocalVar;
 import org.aya.tyck.Result;
 import org.aya.util.Scoped;
@@ -14,7 +13,7 @@ import org.jetbrains.annotations.Nullable;
 /**
  * A locally, lazy substitution<br/>
  * Every substitution should be well-scoped, i.e.,
- * {@link Term} can only refer to some free variable or some lazy substitution in this object.
+ * {@link Result} can only refer to some free variable or elder lazy substitution.
  */
 public record LocalSubstitution(
   @Override @Nullable LocalSubstitution parent,
