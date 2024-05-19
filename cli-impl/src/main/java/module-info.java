93f58576dcfe8a14f3bab22348e4d995e7512998
ice1000 <ice1000kotlin@foxmail.com>
2024-05-19 04:47:25 -0400

    cli-impl: add module-info.java

---
+++ cli-impl/src/main/java/module-info.java
@@ -0,0 +1,25 @@
+module aya.cli.impl {
+  requires transitive aya.parser.ij;
+  requires transitive aya.base;
+
+  requires aya.ij.parsing.core;
+  requires aya.ij.parsing.wrapper;
+  requires com.google.gson;
+  requires aya.md;
+  requires aya.producer;
+  requires org.jetbrains.annotations;
+  requires org.commonmark;
+
+  exports org.aya.cli.interactive;
+  // exports org.aya.cli.library.incremental;
+  exports org.aya.cli.library.json;
+  exports org.aya.cli.library.source;
+  exports org.aya.cli.library;
+  exports org.aya.cli.literate;
+  exports org.aya.cli.render.vscode;
+  exports org.aya.cli.render;
+  exports org.aya.cli.single;
+  exports org.aya.cli.utils;
+
+  opens org.aya.cli.library.json to com.google.gson;
+}
