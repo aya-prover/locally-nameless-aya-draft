2a65692d241261361464699508e9ef1b21896cca
HoshinoTented <hoshinotented@qq.com>
2024-05-19 10:46:23 -0400

    build: gradle.kts
    Co-authored-by: ice1000 <ice1000kotlin@foxmail.com>

---
+++ cli-console/build.gradle.kts
@@ -0,0 +1,17 @@
+// Copyright (c) 2020-2024 Tesla (Yinsen) Zhang.
+// Use of this source code is governed by the MIT license that can be found in the LICENSE.md file.
+import org.aya.gradle.CommonTasks
+
+CommonTasks.nativeImageConfig(project)
+
+dependencies {
+  api(project(":tools-repl"))
+  api(project(":cli-impl"))
+  implementation(project(":producer"))
+  implementation(libs.picocli.runtime)
+  annotationProcessor(libs.picocli.codegen)
+  implementation(libs.jline.terminal.jansi)
+  implementation(libs.jline.builtins)
+  testImplementation(libs.junit.jupiter)
+  testImplementation(libs.hamcrest)
+}
