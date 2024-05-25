789b9f5335e8df9707bfc61eabaa89cadb2fc9b2
HoshinoTented <hoshinotented@qq.com>
2024-05-25 15:16:14 -0400

    compiled: aya

--- syntax/src/main/java/org/aya/syntax/compile/CompiledAya.java
+++ syntax/src/main/java/org/aya/syntax/compile/CompiledAya.java
@@ -2,8 +2,11 @@
 // Use of this source code is governed by the MIT license that can be found in the LICENSE.md file.
 package org.aya.syntax.compile;
 
+import org.aya.syntax.core.repr.AyaShape;
+import org.aya.syntax.core.repr.CodeShape;
 import org.aya.util.binop.Assoc;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -12,6 +15,10 @@ import java.lang.annotation.RetentionPolicy;
 public @interface CompiledAya {
   @NotNull String[] fileModule();
   @NotNull String[] module();
+
   @NotNull String name();
   @NotNull Assoc assoc();
+
+  @Nullable AyaShape shape();
+  @NotNull CodeShape.GlobalId[] recognition();
 }
